731b91bdf6fb62800b8c383b8166df5297607d73
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-01 16:57:20 +0530

    Update ResourceCost.ps1

--- Satya-PowerShell/ResourceCost.ps1
+++ Satya-PowerShell/ResourceCost.ps1
@@ -2,7 +2,7 @@ $startDate = [datetime]::Today.AddDays(-7).ToString('MM/dd/yyy')
 
 $endDate = [datetime]::Today.ToString('MM/dd/yyy')
 
-$resources = Get-AzureRmConsumptionUsageDetail -ResourceGroup ramya -StartDate $startDate -EndDate $endDate
+$resources = Get-AzureRmConsumptionUsageDetail -ResourceGroup <RG Name> -StartDate $startDate -EndDate $endDate
 
 $total 
 foreach ($rg in $resources)
