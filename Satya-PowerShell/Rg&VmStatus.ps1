cf1d04234f5c32a06c852efae41422f5a2e31a9b
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-01 16:46:13 +0530

    Add files via upload

---
+++ Satya-PowerShell/Rg&VmStatus.ps1
@@ -0,0 +1,22 @@
+﻿function Get-ARMVM
+{
+  $RGs = Get-AzureRMResourceGroup
+  foreach($RG in $RGs)
+  {
+    $VMs = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName
+    foreach($VM in $VMs)
+    {
+      $VMDetail = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Status
+      $RGN = $VMDetail.ResourceGroupName  
+      foreach ($VMStatus in $VMDetail.Statuses)
+      { 
+          $VMStatusDetail = $VMStatus.DisplayStatus
+      }
+      Write-Output "Resource Group: $RGN", ("VM Name: " + $VM.Name), "Status: $VMStatusDetail" `n
+    }
+  }
+}
+
+Get-ARMVM
+
+
