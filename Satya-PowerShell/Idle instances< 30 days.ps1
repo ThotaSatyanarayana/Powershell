fcd5b4f32151cb2b00cb45ca5fb0f55b9ea01ee5
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-09 16:17:36 +0530

    Update Idle instances< 30 days.ps1

--- Satya-PowerShell/Idle instances< 30 days.ps1
+++ Satya-PowerShell/Idle instances< 30 days.ps1
@@ -18,8 +18,8 @@ foreach ($vm in $vms) {
     # Calculate the time since the virtual machine was last active
     $idleTime = (Get-Date) - $lastActive
 
-    # Check if the virtual machine has been idle for longer than the threshold
-    if ($status -eq "VM running" -and $idleTime.TotalDays -gt $idleThreshold) {
+    # Check if the virtual machine has been idle for less than the threshold
+    if ($status -eq "VM running" -and $idleTime.TotalDays -lt $idleThreshold) {
         # Output the virtual machine as idle
         Write-Output "Virtual machine '$($vm.Name)' is idle ($($idleTime.TotalDays) days)"
     }
