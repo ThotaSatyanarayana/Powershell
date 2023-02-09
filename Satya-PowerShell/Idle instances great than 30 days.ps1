da741897c16ff242b217da1c3248765efbe3b396
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-09 12:47:12 +0530

    Idle instances great than 30 days

---
+++ Satya-PowerShell/Idle instances great than 30 days.ps1
@@ -0,0 +1,22 @@
+# Set the subscription that contains the snapshots
+Set-AzContext -SubscriptionId "a8cddc3a-fd61-452d-8abe-f7b44b38f5fd"
+
+# Get the list of snapshots in the subscription
+$snapshots = Get-AzSnapshot
+
+# Loop through each snapshot and calculate its age
+foreach ($snapshot in $snapshots) {
+    # Calculate the age of the snapshot
+    $age = [int]((Get-Date) - $snapshot.TimeCreated ).TotalDays
+
+    # Check if the snapshot is older than 30 days
+    if ($age -gt 30) {
+        Write-Output "Snapshot $($snapshot.Name) in resource group $($snapshot.ResourceGroupName) is older than 30 days."
+    }
+    else
+   {
+       Write-Output "Plz check once conditaion days"
+   }
+
+
+}
