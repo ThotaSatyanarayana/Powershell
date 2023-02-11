56e2dcf00babb086589e54c4d96e8fc78cfddccb
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-11 19:32:34 +0530

    Update untagged Resource.ps1

--- Satya-PowerShell/untagged Resource.ps1
+++ Satya-PowerShell/untagged Resource.ps1
@@ -1,4 +1,3 @@
-# Connect to Azure
 Connect-AzAccount
 
 
@@ -7,9 +6,12 @@ $totalResources = (Get-AzResource).Count
 
 # Get the number of untagged resources
 $untaggedResources = (Get-AzResource | Where-Object {-not $_.Tags}).Count
+$untaggedResourceslist = (Get-AzResource | Where-Object {-not $_.Tags})
 
 # Calculate the percentage of untagged resources
 $percentageUntagged = ($untaggedResources / $totalResources) * 100
 
 # Display the result
 Write-Output "Percentage of untagged resources: $percentageUntagged%"
+
+$untaggedResourceslist  | select-object Name,ResourceGroupName,Location,Tags,ResourceType | ft
