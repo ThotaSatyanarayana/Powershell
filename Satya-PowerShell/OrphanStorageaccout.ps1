b85ed957727e1e79e58fc78bb93b36cb6208547d
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-13 15:51:30 +0530

    Update OrphanStorageaccout.ps1
    add more changes

--- Satya-PowerShell/OrphanStorageaccout.ps1
+++ Satya-PowerShell/OrphanStorageaccout.ps1
@@ -20,9 +20,69 @@ $orphanedPercentage = [math]::Round(($orphanedAccounts / $totalAccounts) * 100, 
 Write-Output "Total storage accounts: $totalAccounts" "Orphaned storage accounts: $orphanedAccounts" "Percentage of orphaned storage accounts: $orphanedPercentage%"  | Out-File orphan1.txt
 
 
+................................
+                           # Its Correct
+$orphanedStorageAccounts = @()
+# Get all storage accounts in the subscription
+$storageAccounts = Get-AzStorageAccount
+# Loop through each storage account
+foreach ($storageAccount in $storageAccounts) {
+    $resources = Get-AzResource -ResourceId $storageAccount.Id 
+    # Check if the storage account has a resource group
+    if ($resources.ResourceGroupName -ne $null) {
+        $orphanedStorageAccount = [PSCustomObject]@{
+            'StorageAccountName' = $storageAccount.StorageAccountName
+            'Location' = $storageAccount.Location
+            'ResourceGroupName' = $storageAccount.ResourceGroupName
+            'CreationTime' = $storageAccount.CreationTime
+
+        } 
+        $orphanedStorageAccounts += $orphanedStorageAccount 
+    }
+      
+}
+# Output the list of orphaned storage accounts
+$orphanedStorageAccounts |  Out-File orphanstorage.txt
+
+..................................................
+ # percentage of Orphaned storage account
+ 
+$orphanedStorageAccounts = @()
+# Get all storage accounts in the subscription
+$storageAccounts = Get-AzStorageAccount
+$totalStorageaccounts = $storageAccounts.count
+# Loop through each storage account
+$totalOrphaneAccounts=0
+foreach ($storageAccount in $storageAccounts) {
+    $resources = Get-AzResource -ResourceId $storageAccount.Id 
+
+    # Check if the storage account has a resource group
+    
+ 
+    if ($resources.ResourceGroupName -eq $null) {
+        $totalOrphaneAccounts ++
+        $orphanedStorageAccount = [PSCustomObject]@{
+            'StorageAccountName' = $storageAccount.StorageAccountName
+            'Location' = $storageAccount.Location
+            'ResourceGroupName' = $storageAccount.ResourceGroupName
+            'CreationTime' = $storageAccount.CreationTime
+
+        } 
+        
+        $orphanedStorageAccounts += $orphanedStorageAccount 
+    }
+      
+}
+$orphanedPercentage = [math]::Round(($totalOrphaneAccounts / $totalStorageaccounts) * 100, 2) # Output the results
+# Output the list of orphaned storage accounts
+Write-host "Total Storageaccounts: $totalStorageaccounts  and  Totalorphanedastorages: $totalOrphaneAccounts  PercentageofOrphaned:$orphanedPercentage"
 
 
-# Another Method.........
+$orphanedStorageAccounts |  Out-File orphanstorage7.txt
+.............................
+
+
+              # Another Method.........
 
 $storageAccounts = Get-AzStorageAccount
 $resources = Get-AzResource | Where-Object { $_.Type -eq 'Microsoft.Storage/storageAccounts' }
@@ -30,12 +90,30 @@ $usedStorageAccounts = $resources | Select-Object -ExpandProperty Name
 $orphanedStorageAccounts = $storageAccounts | Where-Object { $_.Name -notin $usedStorageAccounts }
 $orphanedStorageAccounts | Select-Object -Property StorageAccountName,ResourceGroupName,PrimaryLocation,CreationTime 
 
+....................
+$orphanedStorageAccounts = @()
+# Get all storage accounts in the subscription
+$storageAccounts = Get-AzStorageAccount
+# Loop through each storage account
+foreach ($storageAccount in $storageAccounts) {
+    $resources = Get-AzResource -ResourceId $storageAccount.Id 
+    # Check if the storage account has a resource group
+    if ($resources.ResourceGroupName -ne $null) {
+        $orphanedStorageAccount = [PSCustomObject]@{
+            'StorageAccountName' = $storageAccount.StorageAccountName
+            'Location' = $storageAccount.Location
+        }
+        $orphanedStorageAccounts += $orphanedStorageAccount
+    }
+}
+
+
+......................
 
 
 
 
-
-#percentage orphan
+                      #percentage orphan
 
 $storageAccounts = Get-AzStorageAccount
 $resources = Get-AzResource | Where-Object { $_.Type -eq 'Microsoft.Storage/storageAccounts' }
@@ -46,3 +124,32 @@ $totalOrphanedStorageAccounts = $orphanedStorageAccounts.Count
 $percentageOrphanedStorageAccounts = ($totalOrphanedStorageAccounts / $totalStorageAccounts) * 100
 $percentageOrphanedStorageAccounts
 $orphanedStorageAccounts | Select-Object -Property StorageAccountName,ResourceGroupName,PrimaryLocation,CreationTime 
+
+
+
+              # Subscription to subscription move
+
+# Connect to Azure
+Connect-AzAccount
+
+# Get all storage accounts
+$storageAccounts = Get-AzStorageAccount
+
+# Loop through each storage account
+foreach ($storageAccount in $storageAccounts) {
+
+    # Get the subscription id of the storage account
+    # $subscriptionId = $storageAccount.Context.Subscription.Id
+     $subscriptionId =(Get-AzContext).Subscription.Id
+	
+
+
+    # Check if the subscription exists
+    $subscription = Get-AzSubscription -SubscriptionId $subscriptionId -ErrorAction SilentlyContinue
+    if (!$subscriptionId) {
+
+        # If the subscription does not exist, the storage account is considered orphaned
+        Write-Output "Orphaned storage account found: $($storageAccount.StorageAccountName)"
+    }
+}
+
