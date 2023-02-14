4cc3f1a43f18228dff3c7631ec0074d9adea6a4e
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-14 18:50:15 +0530

    Update ResourceGroup-noResoucres.ps1

--- Satya-PowerShell/ResourceGroup-noResoucres.ps1
+++ Satya-PowerShell/ResourceGroup-noResoucres.ps1
@@ -1,5 +1,23 @@
 
 
+# Login to your Azure account
+Connect-AzAccount
+
+# Get a list of all resource groups in the current subscription
+$resourceGroups = Get-AzResourceGroup
+
+# Loop through each resource group and check if it's empty
+foreach ($resourceGroup in $resourceGroups) {
+    $resources = Get-AzResource -ResourceGroupName $resourceGroup.ResourceGroupName
+    if ($resources -eq $null) {
+        Write-Output "Empty resource group: $($resourceGroup.ResourceGroupName)"
+    }
+}
+
+
+#..............................
+
+
 $resourceGroups = Get-AzResourceGroup
 $Header = @("ResourceGroupName","Location")
 $resourceinfo = @()
