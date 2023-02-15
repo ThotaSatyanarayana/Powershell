ab64c8ad73578779142aaf3d53de144546bb58da
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-15 11:36:22 +0530

    Delete Azure-services-and-Totalcount.ps1

--- Azure-services-and-Totalcount.ps1
+++
@@ -1,4 +0,0 @@
-Connect-AzAccount
-$services = Get-AzResource | Group-Object -Property ResourceType | Select-Object -Property Name, Count | Sort-Object -Property Count -Descending | Select-Object -First 10
-$services
-
