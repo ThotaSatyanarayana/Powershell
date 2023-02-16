42300aa2b2de3947e5f2a588ab6e2a52e76157b3
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-16 21:18:04 +0530

    Update Disk-Attached.ps1
    Gb to TB convert

--- Satya-PowerShell/Disk-Attached.ps1
+++ Satya-PowerShell/Disk-Attached.ps1
@@ -26,3 +26,32 @@ if ($disk.DiskState -eq 'Attached' -or $disk.DiskState -eq 'Reserved' ) {
 
 }
 $UnallocatedDisks
+
+#Totacl count Disk and GB to TB Convert
+
+
+$TotalSize = 0
+$UnallocatedDisks =@()
+$disks = Get-AzDisk
+
+# Loop through the disks and filter the unallocated disks
+foreach ($disk in $disks) {
+    $diskName = $disk.Name
+$resourceGroupName = $disk.ResourceGroupName     
+$disk = Get-AzDisk -Name $disk.Name -ResourceGroupName $resourceGroupName
+
+# Check if the disk is allocated
+if ($disk.DiskState -eq 'Reserved'  ) {
+  #  Write-Output "Disk $diskName in resource group $resourceGroupName is currently allocated to a virtual machine"
+  $UnallocatedDisk = [PSCustomObject]@{
+           
+           'DiskSizeGB' = $disk.DiskSizeGB
+           
+       }
+ $TotalSize += $disk.DiskSizeGB 
+   $UnallocatedDisks += $UnallocatedDisk  
+  }
+}
+ $AllDisks =$TotalSize  / 1024
+$Totalcountodsik = $UnallocatedDisks.count
+Write-host "All Unattached disk Count: $Totalcountodsik  and Size: $AllDisks TB"
