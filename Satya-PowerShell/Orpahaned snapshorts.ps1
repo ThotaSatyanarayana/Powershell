cdac714a5b66ba758c18567efed082a40441f4d1
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-16 16:16:02 +0530

    Update Orpahaned snapshorts.ps1

--- Satya-PowerShell/Orpahaned snapshorts.ps1
+++ Satya-PowerShell/Orpahaned snapshorts.ps1
@@ -24,3 +24,39 @@ $orphanedPercentage = [math]::Round(($orphanedSnapshots / $totalSnapshots) * 100
 # Output the results
 Write-Output "Total snapshots: $totalSnapshots"  "Orphaned snapshots: $orphanedSnapshots" "Percentage of orphaned snapshots: $orphanedPercentage%"
 
+# Totalcost BY Month
+
+$orphanedSnapshotlists =@() # Get all orphaned snapshots
+$orphanedSnapshots = Get-AzSnapshot | Where-Object {$_.ManagedBy -eq $null} # Initialize total cost variable
+$totalCost = 0 # Iterate over all orphaned snapshots
+foreach ($snapshot in $orphanedSnapshots) {     # Get the snapshot creation time
+    $snapshotCreateTime = $snapshot.TimeCreated     # Get the snapshot size
+    $snapshotSize = $snapshot.DiskSizeGB     # Calculate the number of days since the snapshot was created
+    $daysSinceCreation = ((get-Date) - $snapshot.TimeCreated).Days     # Calculate the cost of the snapshot
+    $snapshotCost = $snapshotSize * 0.05 * $daysSinceCreation     # Add the snapshot cost to the total cost
+    $totalCost += $snapshotCost 
+  $orphanedSnapshotlist = [PSCustomObject]@{
+SnapshotName =$snapshot.Name
+  SnapshotSize = $snapshotSize
+  TotalCost = $totalCost      }
+$orphanedSnapshotlists += $orphanedSnapshotlist }
+$orphanedSnapshotlists
+
+
+# Totalcost By Days
+
+$orphanedSnapshotlists =@() # Get all orphaned snapshots
+$orphanedSnapshots = Get-AzSnapshot | Where-Object {$_.ManagedBy -eq $null} # Initialize total cost variable
+$totalCost = 0 # Iterate over all orphaned snapshots
+foreach ($snapshot in $orphanedSnapshots) {     # Get the snapshot creation time
+    $snapshotCreateTime = $snapshot.TimeCreated     # Get the snapshot size
+    $snapshotSize = $snapshot.DiskSizeGB     # Calculate the number of days since the snapshot was created
+    $daysSinceCreation = ((get-Date) - $snapshot.TimeCreated).Days     # Calculate the cost of the snapshot
+    $snapshotCost = $snapshotSize * 0.00164 * $daysSinceCreation     # Add the snapshot cost to the total cost
+    $totalCost += $snapshotCost 
+  $orphanedSnapshotlist = [PSCustomObject]@{
+SnapshotName =$snapshot.Name
+  SnapshotSize = $snapshotSize
+  TotalCost = $totalCost      }
+$orphanedSnapshotlists += $orphanedSnapshotlist }
+$orphanedSnapshotlists
