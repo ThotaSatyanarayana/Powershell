8ad6a29ac0a5dd2f2439d06385295d79cb358433
ThotaSatyanarayana <55594529+ThotaSatyanarayana@users.noreply.github.com>
2023-02-17 11:58:23 +0530

    Not using VMs Less then 30 days

--- Satya-PowerShell/Idle instances< 30 days.ps1
+++ Satya-PowerShell/Idle instances< 30 days.ps1
@@ -1,3 +1,47 @@
+
+
+# Get the Azure context
+Connect-AzAccount
+
+# Get the list of all instances in the subscription
+$instances = Get-AzVM
+
+# Loop through the instances and check if they have been idle for the last 30 days
+foreach ($instance in $instances) {
+    # Get the status of the instance
+    $status = (Get-AzVM -ResourceGroupName $instance.ResourceGroupName -Name $instance.Name -Status).Statuses
+
+    # Check if the instance is running and has been idle for the last 30 days
+    if ($status.Code -eq "PowerState/deallocated" -and $status[1].DisplayStatus -eq "VM deallocated") {
+        $lastActivityTime = $status[0].Time
+        $Days = ((Get-date) - $lastActivityTime).Days
+     #$Days
+      if ( $Days -ge 1)
+      {
+        Write-Output "Instance $($instance.Name) in Resource Group $($instance.ResourceGroupName) has been idle for more than 30 days."
+       }
+
+         
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.........................
+
+# Not Correct
+
 # Connect to Azure
 Connect-AzAccount
 
